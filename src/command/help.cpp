4ef20a0acacab00482b211a08fd5e2d5691399b2
Leo Ely <leoely789@gmail.com>
2025-09-29 23:14:52 +0800

    [Change] Change the help tip layout.

--- src/command/help.cpp
+++ src/command/help.cpp
@@ -12,10 +12,8 @@ void help() {
   cout << "  ██████╔╝██║██║░░░░░██║░░░░░███████╗██║░░██║" << endl;
   cout << "  ╚═════╝░╚═╝╚═╝░░░░░╚═╝░░░░░╚══════╝╚═╝░░╚═╝" << endl;
   cout << "" << endl;
-  cout << termcolor::bold << "  Description:" << termcolor::reset << " Generate different target files based on differences and the same template files." << endl;
-  cout << "" << endl;
-  cout << termcolor::bold << "  Command Prompt:" << termcolor::reset << endl;
-  cout << "  - " << termcolor::bold << "generate" << termcolor::reset << endl;
-  cout << "  | " << termcolor::reset << "-" << termcolor::bold << "f" << termcolor::reset << ",--" << termcolor::bold << "file " << termcolor::reset << "[differFile]" << endl;
+  cout << termcolor::bold << "Command line Prompt" << termcolor::reset << ":" << endl;
+  cout << "  - generate Generate multiple difference files based on template files." << endl;
+  cout << "  | " << termcolor::reset << "-" << termcolor::bold << "f" << termcolor::reset << ", --" << termcolor::bold << "file " << termcolor::reset << "[" << termcolor::bold << "differFile" << termcolor::reset << "] Specify the path to the differ template file." << endl;
   cout << "" << endl;
 }
