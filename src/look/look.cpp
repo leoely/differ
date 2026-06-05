fb65e46710aa28568b889c0ec5c8303a7facd1b6
Leo Ely <leoely789@gmail.com>
2026-06-05 18:14:17 +0800

    [Add] Add line number function.

--- src/look/look.cpp
+++ src/look/look.cpp
@@ -17,6 +17,10 @@
 #include <locationTemplate/locationTemplate.hpp>
 #include <variableTemplate/variableTemplate.hpp>
 #include <differTemplate/differTemplate.hpp>
+#include <LocationTokenType/LocationTokenType.hpp>
+#include <DifferTokenType/DifferTokenType.hpp>
+#include <VariableTokenType/VariableTokenType.hpp>
+#include <getWidth/getWidth.hpp>
 
 using std::ifstream;
 using std::string;
@@ -53,6 +57,9 @@ void look(vector<string> arguments) {
         locationParser->scanLine(line);
       }
     }
+    int locationSize = 0;
+    int locationIndex = 0;;
+    vector<shared_ptr<LocationToken>> locationTokens;
     for (const auto& locationOption: locationOptions) {
       shared_ptr<FilePath> filePath(new FilePath(locationOption, ".loc"));
       filePath->dealPath();
@@ -62,10 +69,32 @@ void look(vector<string> arguments) {
       string line;
       while (getline(locationFile, line)) {
         locationLexer->scanLine(line, true);
+        locationSize += 1;
       }
-      vector<shared_ptr<LocationToken>> locationTokens = locationLexer->getTokens();
-      for (const auto& locationToken : locationTokens) {
-        locationTemplate(*locationToken);
+      vector<shared_ptr<LocationToken>> tokens = locationLexer->getTokens();
+      locationTokens.insert(locationTokens.end(), tokens.begin(), tokens.end());
+    }
+    int locationWidth = getWidth(locationSize);
+    for (const auto& locationToken : locationTokens) {
+      LocationToken token = *locationToken;
+      if (locationIndex == 0) {
+        locationIndex += 1;
+        int width = getWidth(locationIndex);
+        string blank = "";
+        for (int i = 0; i < locationWidth - width; i += 1) {
+          blank += "";
+        }
+        cout << termcolor::bold << termcolor::grey << locationIndex << " " << blank;
+      }
+      locationTemplate(token);
+      if (token.type == LocationTokenType::LINE_BREAK && locationIndex != locationSize) {
+        locationIndex += 1;
+        int width = getWidth(locationIndex);
+        string blank = "";
+        for (int i = 0; i < locationWidth - width; i += 1) {
+          blank += " ";
+        }
+        cout << termcolor::bold << termcolor::grey << locationIndex << " " << blank;
       }
     }
   }
